bcc7d4e301aab72d913ad1391a7828239d0d173c
Rodrigo Amaro <rodrigobook@llive.com>
2022-03-18 16:52:01 +0100

    primeiro

---
+++ app.js
@@ -0,0 +1,12 @@
+function abrirmenu(){
+    let barramenuaberto = document.getElementById('barramenu');
+    
+    if(barramenuaberto.style.width =="370px"){
+        barramenuaberto.style.width = "00px";
+    }
+    else{
+        barramenuaberto.style.width = "370px";
+    }
+    
+    }
+    
